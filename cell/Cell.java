0c275302186dad3c5326b1be6b16b694cf86ef67
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-27 11:25:39 +0300

    Implement Cell base class
    Implemented the abstract Cell base class for the Objectville project.
    
    Added:
    - row and column position fields
    - immutable symbol field
    - constructor initialization
    - getter methods for common cell information
    - setter methods for row and column values
    
    This class will serve as the parent class for all map cell types.

--- cell/Cell.java
+++ cell/Cell.java
@@ -1,5 +1,34 @@
 package cell;
 
 public abstract class Cell {
+    private int row;
+    private int col;
+    private final char symbol;
 
+    public Cell(int row, int col, char symbol) {
+        this.row = row;
+        this.col = col;
+        this.symbol = symbol;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
+
