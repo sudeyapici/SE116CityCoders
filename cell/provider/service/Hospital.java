cdfdc12a40f8e5df991e880f9897908c20fb72e4
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 23:32:33 +0300

    Implement Hospital class
    Implemented Hospital as a concrete subclass of ServiceProvider.
    
    Assigned the map symbol 'D' for hospital cells.
    Set the service radius to 3.
    Overrode getServiceType to define health as the provided service.

--- cell/provider/service/Hospital.java
+++ cell/provider/service/Hospital.java
@@ -1,4 +1,13 @@
 package cell.provider.service;
 
-public class Hospital {
+public class Hospital extends ServiceProvider {
+
+    public Hospital(int row, int col) {
+        super(row, col, 'D', 3);
+    }
+
+    @Override
+    public String getServiceType() {
+        return "health";
+    }
 }
