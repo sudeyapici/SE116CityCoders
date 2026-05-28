c553711dee5fa2f6b6410771a4a593197fb1474a
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 13:39:29 +0300

    Implement IndustrialZone class
    Implemented IndustrialZone as a subclass of Zone.
    
    Added fields for received population and produced goods.
    Added method for receiving population resources.
    Overrode resetReceivedValues to clear industrial-specific tick values.
    Implemented industrial level update rules based on electricity, water, population, security, and excess population.
    Implemented goods production based on zone level and minimum utility.
    Updated utility demand according to produced goods.

--- cell/zone/IndustrialZone.java
+++ cell/zone/IndustrialZone.java
@@ -1,4 +1,76 @@
 package cell.zone;
 
-public class IndustrialZone {
+public class IndustrialZone extends Zone {
+    private int producedGoods;
+    private int receivedPopulation;
+
+
+    public IndustrialZone(int row, int col) {
+        super(row, col, 'I');
+        this.receivedPopulation = 0;
+        this.producedGoods = 0;
+    }
+
+    public int getReceivedPopulation() {
+        return receivedPopulation;
+    }
+
+    public int getProducedGoods() {
+        return producedGoods;
+    }
+
+    public void receivePopulation(int amount) {
+        this.receivedPopulation += amount;
+    }
+
+    @Override
+    public void resetReceivedValues() {
+        super.resetReceivedValues();
+        this.receivedPopulation = 0;
+        this.producedGoods = 0;
+    }
+
+
+    @Override
+    public void updateLevel() {
+        if (getReceivedElectricity() == 0 || getReceivedWater() == 0 || receivedPopulation == 0) {
+            setLevel(0);
+        } else {
+            int targetLevel = 1;
+
+            if (hasSecurity()) {
+                targetLevel = 2;
+            }
+
+            int excessPopulation = receivedPopulation - getUtilityDemand();
+
+            if (targetLevel == 2 && excessPopulation > 0) {
+                targetLevel = 3;
+            }
+
+            if (targetLevel > getLevel()) {
+                setLevel(getLevel() + 1);
+            } else if (targetLevel < getLevel()) {
+                setLevel(getLevel() - 1);
+            }
+        }
+    }
+
+
+    @Override
+    public void produce() {
+        calculateMinimumUtility();
+
+        if (getLevel() == 0) {
+            producedGoods = 0;
+        } else if (getLevel() == 1) {
+            producedGoods = getMinimumUtility();
+        } else if (getLevel() == 2) {
+            producedGoods = 2 * getMinimumUtility();
+        } else if (getLevel() == 3) {
+            producedGoods = 2 * getMinimumUtility() + receivedPopulation;
+        }
+
+        setUtilityDemand(producedGoods);
+    }
 }
