d9de3ea06b033009443b8e14c5106784841b9ccc
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 23:30:44 +0300

    Implement ServiceProvider class
    Implemented the abstract ServiceProvider class as a subclass of Cell.
    
    Added final radius field for service coverage range.
    Added constructor initialization for position, symbol, and radius.
    Added getRadius method.
    Added abstract getServiceType method for concrete service provider subclasses.

--- cell/provider/service/ServiceProvider.java
+++ cell/provider/service/ServiceProvider.java
@@ -1,4 +1,20 @@
 package cell.provider.service;
 
-public class ServiceProvider {
+import cell.Cell;
+
+public abstract class ServiceProvider extends Cell {
+
+    private final int radius;
+
+    public ServiceProvider(int row, int col, char symbol, int radius) {
+        super(row, col, symbol);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public abstract String getServiceType();
 }
+
