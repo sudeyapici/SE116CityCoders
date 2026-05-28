a6b85f90a14ff64e434228e3526b72518f811b5b
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 23:33:09 +0300

    Implement School class
    Implemented School as a concrete subclass of ServiceProvider.
    
    Assigned the map symbol 'S' for school cells.
    Set the service radius to 4.
    Overrode getServiceType to define education as the provided service.

--- cell/provider/service/School.java
+++ cell/provider/service/School.java
@@ -1,4 +1,13 @@
 package cell.provider.service;
 
-public class School {
+public class School extends ServiceProvider {
+
+    public School(int row, int col) {
+        super(row, col, 'S', 4);
+    }
+
+    @Override
+    public String getServiceType() {
+        return "education";
+    }
 }
