aa2b151eee9a43c3bb34571bb44ee89f8b8fa66b
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 22:24:51 +0300

    Implement UtilityProvider class
    Implemented the abstract UtilityProvider class as a subclass of Cell.
    
    Added default capacity handling for utility providers.
    Added provideUtility method to supply requested utility amounts based on remaining capacity.
    Added validation for negative requested amounts.
    Added resetCapacity method to restore provider capacity.
    Added abstract getUtilityType method for concrete utility provider subclasses.

--- cell/provider/utility/UtilityProvider.java
+++ cell/provider/utility/UtilityProvider.java
@@ -1,4 +1,41 @@
 package cell.provider.utility;
 
-public class UtilityProvider {
+import cell.Cell;
+
+public abstract class UtilityProvider extends Cell {
+
+    private int capacity;
+
+    public UtilityProvider(int row, int col, char symbol) {
+        super(row, col, symbol);
+        this.capacity = 100;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int provideUtility(int requestedAmount) {
+        if (requestedAmount < 0) {
+            requestedAmount = 0;
+        }
+
+        int givenAmount;
+
+        if (capacity < requestedAmount) {
+            givenAmount = capacity;
+            capacity = 0;
+        } else {
+            givenAmount = requestedAmount;
+            capacity -= requestedAmount;
+        }
+
+        return givenAmount;
+    }
+
+    public void resetCapacity() {
+        this.capacity = 100;
+    }
+
+    public abstract String getUtilityType();
 }
