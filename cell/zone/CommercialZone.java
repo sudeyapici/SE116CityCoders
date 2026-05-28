52bf0c98035b7c16fda526e8138e5b8e12d77325
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 13:40:57 +0300

    Implement CommercialZone class
    Implemented CommercialZone as a subclass of Zone.
    
    Added fields for received population, received goods, and produced lifestyle.
    Added methods for receiving population and goods resources.
    Overrode resetReceivedValues to clear commercial-specific tick values.
    Implemented commercial level update rules based on utilities, population, goods, security, and excess resource checks.
    Implemented lifestyle production based on zone level, minimum utility, and the minimum of received population and goods.
    Updated utility demand according to produced lifestyle.

--- cell/zone/CommercialZone.java
+++ cell/zone/CommercialZone.java
@@ -1,4 +1,90 @@
 package cell.zone;
 
-public class CommercialZone {
+public class CommercialZone extends Zone {
+    private int producedLifestyle;
+    private int receivedPopulation;
+    private int receivedGoods;
+
+    public CommercialZone(int row, int col) {
+        super(row, col, 'C');
+        this.receivedPopulation = 0;
+        this.receivedGoods = 0;
+        this.producedLifestyle = 0;
+    }
+
+    public int getProducedLifestyle() {
+        return producedLifestyle;
+    }
+
+    public int getReceivedPopulation() {
+        return receivedPopulation;
+    }
+
+    public int getReceivedGoods() {
+        return receivedGoods;
+    }
+
+    public void receivePopulation(int amount) {
+        this.receivedPopulation += amount;
+    }
+
+    public void receiveGoods(int amount) {
+        this.receivedGoods += amount;
+    }
+
+
+    @Override
+    public void resetReceivedValues() {
+        super.resetReceivedValues();
+        this.receivedPopulation = 0;
+        this.receivedGoods = 0;
+        this.producedLifestyle = 0;
+    }
+
+
+    @Override
+    public void updateLevel() {
+        if (getReceivedElectricity() == 0 || getReceivedWater() == 0 || getReceivedInternet() == 0 || receivedPopulation == 0 || receivedGoods == 0) {
+            setLevel(0);
+        } else {
+            int targetLevel = 1;
+
+            if (hasSecurity()) {
+                targetLevel = 2;
+            }
+
+            int excessPopulation = receivedPopulation - getUtilityDemand();
+            int excessGoods = receivedGoods - getUtilityDemand();
+
+            if (targetLevel == 2 && excessPopulation > 0 && excessGoods > 0) {
+                targetLevel = 3;
+            }
+
+            if (targetLevel > getLevel()) {
+                setLevel(getLevel() + 1);
+            } else if (targetLevel < getLevel()) {
+                setLevel(getLevel() - 1);
+            }
+        }
+    }
+
+
+    @Override
+    public void produce() {
+        calculateMinimumUtility();
+
+        if (getLevel() == 0) {
+            producedLifestyle = 0;
+        } else if (getLevel() == 1) {
+            producedLifestyle = getMinimumUtility();
+        } else if (getLevel() == 2) {
+            producedLifestyle = 2 * getMinimumUtility();
+        } else if (getLevel() == 3) {
+            producedLifestyle = 2 * getMinimumUtility()
+                    + Math.min(receivedPopulation, receivedGoods);
+        }
+
+        setUtilityDemand(producedLifestyle);
+    }
 }
+
