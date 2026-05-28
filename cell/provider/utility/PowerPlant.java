f4d984951bc74f78f02646f0a20a621698c40eb1
miraymuzac <miray.muzac@std.ieu.edu.tr>
2026-05-28 22:26:18 +0300

    Implement PowerPlant class
    Implemented PowerPlant as a concrete subclass of UtilityProvider.
    
    Assigned the map symbol 'P' for power plant cells.
    Overrode getUtilityType to define electricity as the produced utility.

--- cell/provider/utility/PowerPlant.java
+++ cell/provider/utility/PowerPlant.java
@@ -1,4 +1,13 @@
 package cell.provider.utility;
 
-public class PowerPlant {
+public class PowerPlant extends UtilityProvider {
+
+    public PowerPlant(int row, int col) {
+        super(row, col, 'P');
+    }
+
+    @Override
+    public String getUtilityType() {
+        return "electricity";
+    }
 }
