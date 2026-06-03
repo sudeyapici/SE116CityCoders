1af3f38d1cae7b45fedf812a14dda22d98f5d5b6
sudeyapici <suddeyapici@gmail.com>
2026-06-03 23:33:33 +0300

    Update ServiceDistributor.java

--- cell/distribution/ServiceDistributor.java
+++ cell/distribution/ServiceDistributor.java
@@ -1,36 +1,28 @@
-package cell.distribution;
+package distribution;
 
 import cell.Cell;
 import cell.provider.service.ServiceProvider;
 import cell.zone.Zone;
 import map.CityMap;
+import simulation.OutputPrinter;
 
 public class ServiceDistributor {
 
-    // Finds all ServiceProvider cells on the map.
-    // Service distribution is not done with BFS.
-    // Because according to the directive, the services do not want a connection, it only works within the radius.
-
-
-    public void distributeServices(CityMap cityMap) {
+    public void distributeServices(CityMap cityMap, OutputPrinter outputPrinter) {
         for (int row = 0; row < cityMap.getRows(); row++) {
             for (int col = 0; col < cityMap.getCols(); col++) {
                 Cell cell = cityMap.getCell(row, col);
 
                 if (cell instanceof ServiceProvider) {
                     ServiceProvider provider = (ServiceProvider) cell;
-
-                    distributeServiceFromProvider(cityMap, provider);
+                    distributeServiceFromProvider(cityMap, provider, outputPrinter);
                 }
             }
         }
     }
 
-// Finds the zones in the radius for a single service provider.
-// I used Manhattan distance.So distance = |row difference| + |col difference|
-
-
-    private void distributeServiceFromProvider(CityMap cityMap, ServiceProvider provider) {
+    private void distributeServiceFromProvider(CityMap cityMap, ServiceProvider provider,
+                                               OutputPrinter outputPrinter) {
         int providerRow = provider.getRow();
         int providerCol = provider.getCol();
         int radius = provider.getRadius();
@@ -45,21 +37,16 @@ public class ServiceDistributor {
                     if (distance <= radius) {
                         Zone zone = (Zone) cell;
                         giveServiceToZone(zone, provider.getServiceType());
+                        outputPrinter.printServiceReceived(zone, provider.getServiceType());
                     }
                 }
             }
         }
     }
 
-
     private int calculateManhattanDistance(int row1, int col1, int row2, int col2) {
         return Math.abs(row1 - row2) + Math.abs(col1 - col2);
     }
-
-
-    // security -> receiveSecurity
-    // health -> receiveHealth
-    // education -> receiveEducation
 
     private void giveServiceToZone(Zone zone, String serviceType) {
         if (serviceType.equals("security")) {
